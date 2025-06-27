e44339b89c02f344826f09efa84563f5b7f019c4
Simrat Kaur <simratk.cs.22@nitj.ac.in>
2025-06-27 20:36:45 +0530

    Test Vercel deployment render

--- src/App.js
+++ src/App.js
@@ -26,33 +26,50 @@
 
 // export default App;
 
-import React, { useRef} from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 
-import Navbar from "./components/navbar.component";
-import ExerciseList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercises.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
-import Footer from './footer';
+
+
+
+
+// import React, { useRef} from 'react';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+
+// import Navbar from "./components/navbar.component";
+// import ExerciseList from "./components/exercises-list.component";
+// import EditExercise from "./components/edit-exercises.component";
+// import CreateExercise from "./components/create-exercise.component";
+// import CreateUser from "./components/create-user.component";
+// import Footer from './footer';
+
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="container">
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<ExerciseList />} />
+//           <Route path="/edit/:id" element={<EditExercise />} />
+//           <Route path="/create" element={<CreateExercise />} />
+//           <Route path="/user" element={<CreateUser />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ExerciseList />} />
-          <Route path="/edit/:id" element={<EditExercise />} />
-          <Route path="/create" element={<CreateExercise />} />
-          <Route path="/user" element={<CreateUser />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <h2>Hello from Vercel Frontend!</h2>
+    </div>
   );
 }
 
