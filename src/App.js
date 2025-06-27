efbe16dde92f19a897f413e7785f156ce4330acd
Simrat Kaur <simratk.cs.22@nitj.ac.in>
2025-06-27 20:46:57 +0530

    Updated Axios calls and added .env for deployment

--- src/App.js
+++ src/App.js
@@ -32,45 +32,45 @@
 
 
 
-// import React, { useRef} from 'react';
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useRef} from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
-// import Navbar from "./components/navbar.component";
-// import ExerciseList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercises.component";
-// import CreateExercise from "./components/create-exercise.component";
-// import CreateUser from "./components/create-user.component";
-// import Footer from './footer';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="container">
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<ExerciseList />} />
-//           <Route path="/edit/:id" element={<EditExercise />} />
-//           <Route path="/create" element={<CreateExercise />} />
-//           <Route path="/user" element={<CreateUser />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+import Footer from './footer';
 
 
 function App() {
   return (
-    <div>
-      <h2>Hello from Vercel Frontend!</h2>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ExerciseList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>Hello from Vercel Frontend!</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
